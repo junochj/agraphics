f32d8969b0af53bc7b31b97bccb734be4c8ed76e
junochj <90077300+junochj@users.noreply.github.com>
2022-01-17 12:08:05 +0900

    Add files via upload

---
+++ motion.js
@@ -0,0 +1,13 @@
+﻿$(function(){
+  //헤더 메뉴 부분
+   $("nav>ul>li>ul").hide();
+   $("nav>ul>li h2").click(function(){
+      $("nav>ul>li ul").fadeOut()
+      $(this).next().fadeIn();
+   }); 
+
+  //이벤트 바네이 갤러리 부분
+    $(".btn>li").click( function(){  
+       $(".event_visual_list").animate({ marginLeft: -500 * $(this).index() });
+     } );
+});
